2ae5d71bb411f9fce89e851eef1680b7f53e437d
mabadejedaniel1 <mabadejedaniel1@gmail.com>
2019-09-18 20:16:11 -0700

    added sql file

---
+++ database/sql_database_script.sql
@@ -0,0 +1,31 @@
+-- MYSQL Script ---
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+-- Database: `default_database`
+CREATE DATABASE `hng`;
+
+-- Select database
+USE default_database;
+
+
+-- Table structure for table `user`
+CREATE TABLE `user` (
+  `_id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `password` varchar(400) NOT NULL,
+  `email` varchar(255) NOT NULL,
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+ 
+
+-- Indexes for table `user`
+ALTER TABLE `user`
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`);
+
+COMMIT;
